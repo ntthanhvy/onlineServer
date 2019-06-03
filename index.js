c0d21476cdc1bc13ead4c96ae3ce78e5b0072b9f
nthanhvy <nguyenthithanhvyjps15@gmail.com>
2019-06-03 21:20:13 +0700

    test on server

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ const app = express()
 app.get('/', (req, res) => {
   res.send('HEY!')
 })
+
+app.get('/player', (req, res) => {
+    res.send('This should get the player')
+})
 app.listen(3000, () => console.log('Server running on port 3000'))  
