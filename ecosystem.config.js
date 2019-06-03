c430d4642171691f11737277c690a63bc294d2e1
nthanhvy <nguyenthithanhvyjps15@gmail.com>
2019-06-03 20:56:29 +0700

    setup pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    apps: [{
+      name: 'tutorial-2',
+      script: './index.js'
+    }],
+    deploy: {
+      production: {
+        user: 'ubuntu',
+        host: 'ec2-18-188-89-217.us-east-2.compute.amazonaws.com',
+        key: '~/.ssh/onlineAPIServer.pem',
+        ref: 'origin/master',
+        repo: 'git@github.com:ntthanhvy/onlineServer.git',
+        path: '/home/ubuntu/onlineServer',
+        'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+      }
+    }
+  }
