81b8d72b1390309c08d3c81e564e3f4b9a055ef1
nthanhvy <nguyenthithanhvyjps15@gmail.com>
2019-06-04 09:54:55 +0700

    add body-parser

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express')
+const bodyParser = require('body-parser');
+
 const app = express()
+
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: true
+    })
+)
+
 app.get('/', (req, res) => {
-  res.send('HEY!')
+  res.json({ info: 'Node.js, Express and Postgres API'})
 })
 
 app.get('/player', (req, res) => {
-    res.send('This should get the player')
+    res.json({'message': 'This should get the player'})
 })
 app.listen(3000, () => console.log('Server running on port 3000'))  
