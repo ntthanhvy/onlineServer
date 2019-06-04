f4b0dda867d36af5954178718e14633d0d84ed04
nthanhvy <nguyenthithanhvyjps15@gmail.com>
2019-06-04 10:09:49 +0700

    test on post request

--- index.js
+++ index.js
@@ -17,4 +17,8 @@ app.get('/', (req, res) => {
 app.get('/player', (req, res) => {
     res.json({'message': 'This should get the player'})
 })
+
+app.post('/player', (req, res) => {
+    res.json({ 'message': 'This should create new player'})
+})
 app.listen(3000, () => console.log('Server running on port 3000'))  
