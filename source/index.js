69626c1192e09e6e622c085f678545d467a30979
nthanhvy <nguyenthithanhvyjps15@gmail.com>
2019-06-04 11:15:04 +0700

    add connection to database

--- source/index.js
+++ source/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser');
+const Pool = require('pg').Pool;
 
 const app = express()
 
@@ -10,6 +11,14 @@ app.use(
     })
 )
 
+const db = new Pool({
+    user: process.env.ADMISSION_DB_USER,
+    host: '18.188.89.217',
+    port: 5432,
+    database: 'farcryonline',
+    password: null
+});
+
 app.get('/', (req, res) => {
   res.json({ info: 'Node.js, Express and Postgres API'})
 })
@@ -19,6 +28,11 @@ app.get('/player', (req, res) => {
 })
 
 app.post('/player', (req, res) => {
-    res.json({ 'message': 'This should create new player'})
+    try {
+        const { rows } = db.query("SELECT * FROM player");
+        return res.status(200).json(rows)
+    } catch (error) {
+        return res.status(400).json(error)
+    }
 })
 app.listen(3000, () => console.log('Server running on port 3000'))  
